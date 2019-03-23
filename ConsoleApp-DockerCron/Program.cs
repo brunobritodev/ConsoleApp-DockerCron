ec28570d9eaacb176ede6e6c5ad1fb9e870dad4b
Bruno Brito <bhdebrito@gmail.com>
2019-03-22 23:59:29 -0300

    alfa

--- ConsoleApp-DockerCron/Program.cs
+++ ConsoleApp-DockerCron/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace ConsoleApp_DockerCron
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(params string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            try
+            {
+                File.AppendAllText("log", $"I'm alive: {DateTime.Now}{Environment.NewLine}");
+
+                File.WriteAllText("healtcheck", "1");
+            }
+            catch (Exception)
+            {
+                File.WriteAllText("healtcheck", "0");
+                Environment.ExitCode = 1;
+            }
+            
         }
     }
 }
